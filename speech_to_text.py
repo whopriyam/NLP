be1fbbf5586a4987ed76b565810ea58bf374a7b5
whopriyam <priyambasu16@gmail.com>
2020-03-18 23:07:22 +0530

    Add existing file

---
+++ speech_to_text.py
@@ -0,0 +1,11 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Speak Anything :")
+    audio = r.listen(source)
+    try:
+        text = r.recognize_google(audio)
+        print("Speech : {}".format(text))
+    except:
+        print("Try again. Could not recognise :p")
